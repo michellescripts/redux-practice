13d83263ad1fd4a2a22edb6b1c31cf79f4dfa3e4
Michelle Bergquist <caker25@gmail.com>
2017-07-06 11:06:25 -0600

    Redux counter

---
+++ bodyClick/bodyClick.js
@@ -0,0 +1,24 @@
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const { createStore } = Redux // does as above
+const store = createStore(counter)
+
+const render = () => {
+  document.body.innerText = store.getState()
+}
+
+store.subscribe(render)
+render()
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' })
+})
